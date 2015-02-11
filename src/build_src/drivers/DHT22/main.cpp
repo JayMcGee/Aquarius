75ebfb1319e8aad445a35e8df7c9c193f6eb93d0
Cheroukee <fournierje.info@gmail.com>
2015-02-11 11:44:14 -0500

    First draft of driver communications

--- src/build_src/drivers/DHT22/main.cpp
+++ src/build_src/drivers/DHT22/main.cpp
@@ -1,15 +1,40 @@
 #include "dht22Device.h"
+#include <string.h>
 #include <iostream>
 #include <unistd.h>
+#include <stdlib.h>
+#include <vector>
 
 using namespace std;
 
-int main()
+//string stringify(int argc, char ** argv);
+const vector<string> explode(const string& s, const char& c);
+
+int main(int argc, char ** argv)
 {
     float humidite = 0;
     float temperature = 0;
     
-    DHT dht(0, 26);
+    if(argc < 3)
+    {
+        cout << "Missing arguments, could not execute" << endl
+             << "[Executable] [DeviceLocation] [Command]" << endl;
+        return 1;
+    }
+    
+    string deviceLocation;
+    deviceLocation.assign(argv[1]);
+    
+    string command;
+    command.assign(argv[2]);
+    
+    cout << "Emplacement : " << deviceLocation << endl
+         << "Commande : " << command << endl;
+    
+    vector<string> v{explode(deviceLocation, ':')};
+    int high = atoi((v[0].c_str())), low = atoi((v[1].c_str()));
+    DHT dht(high, low);
+    
     
     int retour = 0;
     
@@ -41,5 +66,28 @@ int main()
         }
         sleep(2);
     }
+    
     return 0;
+}
+/*
+std::string[] stringify(int argc, char ** argv)
+{
+    std::string args[argc];
+    
+	return args;
+}*/
+
+const vector<string> explode(const string& s, const char& c)
+{
+	string buff{""};
+	vector<string> v;
+	
+	for(auto n:s)
+	{
+		if(n != c) buff+=n; else
+		if(n == c && buff != "") { v.push_back(buff); buff = ""; }
+	}
+	if(buff != "") v.push_back(buff);
+	
+	return v;
 }
