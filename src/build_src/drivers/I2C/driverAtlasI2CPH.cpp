fe84c614ecbf19129ef4ccf295dbafd06c7a151b
Cheroukee <fournierje.info@gmail.com>
2015-02-14 16:08:22 -0500

    Continued work on PH driver
    Added zone for all command except transfer to serial mode. Added a lot
    of defines to aid in coding. Simplified the command identification,
    refactored some variables to some more expletive names

--- src/build_src/drivers/I2C/driverAtlasI2CPH.cpp
+++ src/build_src/drivers/I2C/driverAtlasI2CPH.cpp
@@ -5,27 +5,51 @@
 
 #define I2C_COMMS_ERROR "I2C_COMMS_ERROR"
 
-#define COMMAND_CALIB "Cal"
-#define COMMAND_I "I"
-#define COMMAND_L "L"
-#define COMMAND_R "R"
-#define COMMAND_SLEEP "Sleep"
-#define COMMAND_STATUS "Status"
-#define COMMAND_T "T"
-#define COMMAND_X "X"
+//Commands that can be called for the pH module
+#define PH_COMMAND_CALIB "Cal"
+#define PH_COMMAND_I "I"
+#define PH_COMMAND_L "L"
+#define PH_COMMAND_R "R"
+#define PH_COMMAND_SLEEP "Sleep"
+#define PH_COMMAND_STATUS "Status"
+#define PH_COMMAND_T "T"
+#define PH_COMMAND_X "X"
 
-#define COMMAND_MID "mid"
-#define COMMAND_HIGH "high"
-#define COMMAND_LOW "low"
-#define COMMAND_CLEAR "clear"
+//Calibration parameters
+#define PH_COMMAND_MID "mid"
+#define PH_COMMAND_HIGH "high"
+#define PH_COMMAND_LOW "low"
+#define PH_COMMAND_CLEAR "clear"
 
-#define COMMAND_CALIB_DELAY 13
-#define COMMAND_R_DELAY 10
+//Command delay before read
+#define PH_COMMAND_CALIB_DELAY 13
+#define PH_COMMAND_R_DELAY 10
+#define PH_COMMAND_L_DELAY 3
+#define PH_COMMAND_STATUS_DELAY 3
+#define PH_COMMAND_SLEEP_DELAY 3 
+#define PH_COMMAND_I_DELAY 3
+#define PH_COMMAND_X_DELAY 3
+#define PH_COMMAND_T_DELAY 3
+
+#define PH_COMMAND_ARG_QUEST "?"
 
 using namespace BlackLib;
 using namespace std;
 
+/**
+ * @brief Function that executes a command on the i2c, waiting the delay for an answer to be put in return
+ * @param i2c			I2C object that will point to the good i2c bus
+ * @param commandTo     Command to send to the device
+ * @param delay			Delay before looking for an answer
+ * @param returnData    Pointer to the returned data buffer
+ */
 void getStatus(BlackI2C * i2c, string commandTo, int delay, string * returnData);
+
+/**
+ * @brief Output the missing arguments phrase for help
+ * @return A string that explains the minimum args for the driver to work
+ */
+string AtlasPH_output_missingArguments();
 
 int main(int argc, char * argv[])
 {
@@ -33,16 +57,15 @@ int main(int argc, char * argv[])
     
     if(argc < 3)
     {
-        cout << "Missing arguments, could not execute" << endl
-             << "[Executable] [bus:address] [command]" << endl;
+        cout << AtlasPH_output_missingArguments << endl;
         return 1;
     }
     string deviceLocation;
     deviceLocation.assign(argv[1]);
     
-    vector<string> v{aquarius::splitArguments(deviceLocation, ':')};
+    vector<string> deviceLocationSplitted{aquarius::splitArguments(deviceLocation, ':')};
     
-    if(v.size() < 2)
+    if(deviceLocationSplitted.size() < 2)
     {
         cout << "Not enough I2C location information, must be [bus:address]" << endl;  
         return 1;
@@ -50,7 +73,7 @@ int main(int argc, char * argv[])
     
     BlackLib::i2cName bus;
     
-    int bus_number = atoi(v[0].c_str());
+    int bus_number = atoi(deviceLocationSplitted[0].c_str());
     
     if(bus_number == 0)
     {
@@ -66,7 +89,7 @@ int main(int argc, char * argv[])
         return 1;
     }
     
-    int address = atoi(v[1].c_str());
+    int address = atoi(deviceLocationSplitted[1].c_str());
     
     if(address > 255 && address <= 0)
     {
@@ -78,44 +101,99 @@ int main(int argc, char * argv[])
     myI2c.open( BlackLib::ReadWrite | BlackLib::NonBlock);
     
     string command;
-    command.assign(argv[2]);
-    
-    vector<string> c{aquarius::splitArguments(command, ':')};
-    
-    if(c.size() > 1)
-    {
-        string finalCommand;
-        if(c[0].compare(COMMAND_CALIB) == 0)
-        {
-            if(c.size() > 2 && (c[2].compare(COMMAND_MID) == 0 || c[2].compare(COMMAND_HIGH) == 0 || c[2].compare(COMMAND_LOW) == 0))
-            {
-                finalCommand = (string)c[0] + "," + c[1] + "," + c[2];
-            }
-            else
-            {
-                finalCommand = (string)c[0] + "," + c[1];
-            }
-            cout << "Final command : " << finalCommand << endl;
-            getStatus(&myI2c, finalCommand, COMMAND_CALIB_DELAY, &returnString);
-        }
-    }
-    //Dans le cas d'un reading
-    else if(command.compare(COMMAND_R) == 0)
-    {
-        getStatus(&myI2c, command, COMMAND_R_DELAY, &returnString);
-    }
-    
+    command.assign(argv[2]);    
+    vector<string> commandSplitted{aquarius::splitArguments(command, ':')};
+	
+    ////////////////////////////********COMMAND BUILDING**********////////////////////
+    //Creates i2c command
+	//If command is calibration 	
+	if(commandSplitted[0].compare(PH_COMMAND_CALIB) == 0)
+	{
+		//Verify that there is the minimum second argument
+		if(commandSplitted.size() > 1)
+		{
+			string finalCommand;
+			//If there is 3 arguments total, verifiy their validity (mid, high or low calibration)
+			if(commandSplitted.size() > 2 && (commandSplitted[2].compare(PH_COMMAND_MID) == 0 ||
+								commandSplitted[2].compare(PH_COMMAND_HIGH) == 0 ||
+								commandSplitted[2].compare(PH_COMMAND_LOW) == 0))
+			{
+				finalCommand = (string)commandSplitted[0] + "," + commandSplitted[1] + "," + commandSplitted[2];
+			}
+			//If there is only 2 arguments, verify validity (clear or question mark)
+			else if(commandSplitted[1].compare(PH_COMMAND_CLEAR) == 0 ||
+					commandSplitted[1].compare(PH_COMMAND_ARG_QUEST) == 0)
+			{
+				finalCommand = (string)commandSplitted[0] + "," + commandSplitted[1];
+			}
+			else
+			{
+				//OUTPUT ERROR NOT GOOD SUB COMMAND
+				return 1;
+			}
+			cout << "Final command : " << finalCommand << endl;
+			i2cCommand(&myI2c, finalCommand, PH_COMMAND_CALIB_DELAY, &returnString);
+		}
+		else
+		{
+			//OUTPUT ERROR NOT ENOUGH ARGS
+			return 1;
+		}
+	}   
+	else if(commandSplitted[0].compare(PH_COMMAND_X) == 0)
+	{
+		cout << "Initiating a factory reset" << endl;
+		i2cCommand(&myI2c, commandSplitted[0], PH_COMMAND_X_DELAY, &returnString);
+		i2cCommand(&myI2c, PH_COMMAND_STATUS, PH_COMMAND_STATUS_DELAY, &returnString);
+	}
+	else if(commandSplitted[0].compare(PH_COMMAND_L) == 0)
+	{
+		if(commandSplitted.size() > 1)
+		{
+			
+		}
+		else
+		{
+			//OUTPUT ERROR NOT ENOUGH PARAMS
+			return 1;
+		}			
+		//i2cCommand(&myI2c, command, PH_COMMAND_R_DELAY, &returnString);
+	}
+	else if(commandSplitted[0].compare(PH_COMMAND_T) == 0)
+	{
+		if(commandSplitted.size() > 1)
+		{
+			
+		}
+		else
+		{
+			//OUTPUT ERROR NOT ENOUGH PARAMS
+			return 1;
+		}
+	}	
+	else if(commandSplitted[0].compare(PH_COMMAND_I) == 0)=
+		i2cCommand(&myI2c, commandSplitted[0], PH_COMMAND_R_DELAY, &returnString);	
+    //If the command is a reading
+    else if(commandSplitted[0].compare(PH_COMMAND_R) == 0)
+        i2cCommand(&myI2c, commandSplitted[0], PH_COMMAND_R_DELAY, &returnString);
+	else if(commandSplitted[0].compare(PH_COMMAND_STATUS) == 0)
+		i2cCommand(&myI2c, commandSplitted[0], PH_COMMAND_STATUS_DELAY, &returnString);
+	else if(commandSplitted[0].compare(PH_COMMAND_SLEEP) == 0)
+		i2cCommand(&myI2c, commandSplitted[0], PH_COMMAND_SLEEP_DELAY, &returnString);
+	////////////////////////////********END OF COMMAND*************//////////
+	
+    ////////////////////////////********RESPONSE ANALYSIS**********////////////////////
     //Outputs designed for each commands
     if(returnString.compare(I2C_COMMS_ERROR) == 0)
     {
         aquarius::outputError("PH", I2C_COMMS_ERROR);
     }
-    else if(c[0].compare(COMMAND_CALIB) == 0)
+    else if(commandSplitted[0].compare(PH_COMMAND_CALIB) == 0)
     {
         cout << "Command calibration executed" << returnString << endl;
         
     }
-    else if(c[0].compare(COMMAND_R) == 0)
+    else if(commandSplitted[0].compare(PH_COMMAND_R) == 0)
     {
         cout << "Command succesfully executed : " << returnString<< endl;
         string dataName[] = { "PH" };
@@ -123,18 +201,14 @@ int main(int argc, char * argv[])
         aquarius::outputReadData("PH", 1, dataName, datas);
         return 0;
     }
-    
+    /////////////////////////////////**************END OF RESPONSE****////////////////
+	
     return 1;
 }
 
-void getStatus(BlackI2C * i2c, string commandTo, int delay, string * returnData)
+void i2cCommand(BlackI2C * i2c, string commandTo, int delay, string * returnData)
 {
     uint8_t buffer[32] = {0x00};
-    //uint8_t sendBuffer[6] = { 'S', 'T', 'A', 'T', 'U', 'S'};
-    //uint8_t sendBuffer[1] = { 'I'};
-    //uint8_t sendBuffer[3] = {'K', ',', '?'};
-    //uint8_t sendBuffer[3] = {'K', ',', '?'};
-    //uint8_t sendBuffer[5] = {'C', 'A', 'L', ',', '?'};
     
     i2c->writeLine((uint8_t*)commandTo.c_str(),commandTo.size());
     
@@ -148,4 +222,9 @@ void getStatus(BlackI2C * i2c, string commandTo, int delay, string * returnData)
     {
         (*returnData) = I2C_COMMS_ERROR;
     }
+}
+
+string AtlasPH_output_missingArguments()
+{
+	return "Missing arguments, could not execute \n\r [Executable] [bus:address] [command]";
 }
