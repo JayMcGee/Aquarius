744e33a8c0018e81d9db88c34f6fc5eabcb89645
root <root@beaglebone.localdomain>
2015-03-01 21:40:19 +0000

    Refactored some details and corrected errors on install script

--- install.sh
+++ install.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
-
-sudo apt-get update
-sudo apt-get install build-essential python-dev python-setuptools python-pip python-smbus -y
-sudo apt-get install mysql-server mysql-client -y
+apt-get update; apt-get install build-essential python-dev python-setuptools python-pip python-smbus -y
+apt-get install mysql-server mysql-client -y
 echo "Installing npm packages ..."
 cd /var/lib/cloud9/Aquarius/node-bootstrap
-npm install 
+npm install
 echo "Installing Express.io ... "
 npm install express.io
 echo "Installing node-schedule ... "
 npm install node-schedule
 echo "Installing execSync ... "
 npm install execSync
-
 echo "Installing aquarius.service to system ..."
 cp /var/lib/cloud9/Aquarius/aquarius.service /lib/systemd/system
 cd /etc/systemd/system/multi-user.target.wants/
 ln /lib/systemd/system/aquarius.service
-
 cd /var/lib/cloud9/Aquarius
-
 mkdir /mnt/card
 mount /dev/mmcblk0p1  /mnt/card
 cp /var/lib/cloud9/Aquarius/install/uEnv.txt /mnt/card/uEnv.txt
-
 dtc -O dtb -o /var/lib/cloud9/Aquarius/install/w1-00A0.dtbo -b 0 -@ /var/lib/cloud9/Aquarius/install/w1.dts
-
 cp /var/lib/cloud9/Aquarius/install/w1-00A0.dtbo /lib/firmware
-
 cp /var/lib/cloud9/Aquarius/install/rc.local /etc
-
 echo "Thy shall reboot, for thy need to reseat"
 reboot
