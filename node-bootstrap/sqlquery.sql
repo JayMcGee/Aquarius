0a1c4c43f9d98360a0ce27e5c91d960d8ec0978e
Cheroukee <fournierje.info@gmail.com>
2015-03-01 12:51:56 -0500

    Ajout d'un fichier avec un insert dans la base de données

---
+++ node-bootstrap/sqlquery.sql
@@ -0,0 +1,5 @@
+USE('aquariusStation');
+
+INSERT INTO `sensorData` 
+	(date, water_temp, water_conduc, water_do, water_ph, case_temp, case_humidity) 
+VALUES ('ladate', 'wtemp', 'wk', 'wdo', 'wph', 'ctemp', 'chum');
