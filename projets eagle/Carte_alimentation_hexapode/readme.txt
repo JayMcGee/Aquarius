e9c408f18760828047afef33297502926723b753
Jean-Philippe Fournier <fournierje.info@gmail.com>
2015-03-18 09:16:27 -0400

    Deleted some deprecated folders

--- projets eagle/Carte_alimentation_hexapode/readme.txt
+++
@@ -1,3 +0,0 @@
-Logiciel de PCB utilisé : EAGLE 6.5.0 pro
-
-bibliothèque des composants utilisés disponible dans le répertoire introlab_library
--- src/clock_init_fromBBB.sh
+++
@@ -1,4 +0,0 @@
-echo ds3231 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
-hwclock -w -f /dev/rtc1
-date
-echo 0x68 > /sys/class/i2c-adapter/i2c-1/delete_device
