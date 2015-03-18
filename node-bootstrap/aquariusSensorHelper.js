612ddfa5447bdf8ae23fd388e86507f9168d9033
Jean-Philippe Fournier <fournierje.info@gmail.com>
2015-03-18 12:57:53 -0400

    Added new configuration set and get to aquariusSensorHelper
    Signed-off-by: Jean-Philippe Fournier <fournierje.info@gmail.com>

--- node-bootstrap/aquariusSensorHelper.js
+++ node-bootstrap/aquariusSensorHelper.js
@@ -1,54 +1,22 @@
 var mysql = require('mysql');
-//var db_types = require('./db_types')
-//var db_config = require('./db_config')
-
 
 module.exports = {
     
-    
-    readConfig : function( connection , callbackToApp ){
+    readConfig : function( connection , callBackToApp ){
         console.log("Selecting database")
-        connection.query('USE `station_aquarius`;');
-        console.log("Querying")
+        connection.query('USE `station_aquarius`;')
         
-        connection.query('SELECT config_id AS ID, config_key_name AS Name, config_key_value AS Value FROM `t_Config`;', callbackToApp)
-    }
+        console.log("Querying")
+        connection.query('SELECT config_id AS ID, config_key_name AS Name, config_key_value AS Value FROM `t_Config`;', callBackToApp)
+    },
     
-    
-    /*
-  readAllSensorsToDB : function( connection ){
-      console.log("In readAllSensors")
-      db_types.getSensors(connection , type_ID , iterateSensors)
-    },
-
-    iterateSensors : function ( connection , sensors ){
-      console.log("In iterateSensorsToDB")
-      db_types.getDriverPaths( connection , sensors , iterateSensorsToDB )
-    },
-
-    iterateSensorsToDB : function ( connection , sensors , types ){
-
-      var path
-      for( var i in sensors ){
-        for ( var x in types ){
-          if(i.unit_t_type == x.types_id)
-          {
-            path = x.types_driver
-          }
-        }
-
-        stdout = execSync(path + " "  + sensors.unit_address + " R")
-        db_types.getSensorReadings(connection, stdout, sensors.units_id, iterateUnitsToDB)
-
-      }
-
-
-    },
-
-    iterateUnitsToDB : function( connection , units , output ){
-      for(var i in units){
-        //Extract data and put in db
-      }
+    setConfig : function( connection , name , value , callBackToApp){
+        console.log("Selecting database")
+        connection.query('USE `station_aquarius`;')
+        
+        console.log("Querying")
+        sql = 'UPDATE `t_Config` SET `config_key_value` = ' + value + ' WHERE `config_key_name` = "' + name + '";';
+        console.log(sql)
+        connection.query(sql, callBackToApp)
     }
-*/
 };
