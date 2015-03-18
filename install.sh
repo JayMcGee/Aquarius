b2cc726722b32e162a815c5b4041338764823d8a
root <jean.pascal.mcgee@gmail.com>
2015-03-18 10:02:12 +0000

    Added Install Script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "install npm packages"
+cd /var/lib/cloud9/Aquarius/node-bootstrap
+npm install 
+npm install express.io
+npm install node-schedule
+
+echo "installing Service"
+cp /var/lib/cloud9/Aquarius/aquarius.service /lib/systemd/system
+cd /etc/systemd/system/multi-user.target.wants/
+ln /lib/systemd/system/aquarius.service
+
+cd /var/lib/cloud9/Aquarius
