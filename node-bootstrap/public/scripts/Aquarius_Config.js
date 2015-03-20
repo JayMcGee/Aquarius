2f50261529d2ae43ba618b5edc98f05901a1a745
root <jean.pascal.mcgee@gmail.com>
2015-03-20 11:17:56 +0000

    Form.html update

--- node-bootstrap/public/scripts/Aquarius_Config.js
+++ node-bootstrap/public/scripts/Aquarius_Config.js
@@ -9,3 +9,67 @@
  * @date : 20 MAR 2015
  * @version : 0.1.1
 */
+
+io = io.connect()
+io.emit('ready')
+
+io.emit('requestConfig')
+
+io.on('receiveConfig',function(data)
+	{
+	    var dbRows = data.rows
+	    
+	    for ( var i = 0; i < dbRows.length; i++)
+	    {
+	        var id    = dbRows[i].Id
+	        var name  = dbRows[i].Name
+	        var value = dbRows[i].Value
+	        var desc  = dbRows[i].Description
+	        
+	        if(name == "STATION_ID")
+	        {
+	            $("#idInput").val(value)
+	        }
+	        else if(name == "NUMBER_RETRIES")
+	        {
+	             $("#retriesInput").val(value)
+	        }
+	        else if(name == "SEND_ADDRESS")
+	        {
+	            $("#addressInput").val(value)
+	        }
+	        else if (name == "READ_INTERVAL")
+	        {
+	         	$("#intervalInput").val(value)   
+	        }
+	        else if (name == "LAST_KNOWN_DATE")
+	        {
+	            $("#dateInput").html(value)
+	        }
+	    }
+	})
+
+
+/**
+ * @brief Send keyName and Value to update configuration
+ * @param KeyName (string)
+ * @param Value (var)
+ */
+function configSave(){
+    
+}
+
+
+
+$("#saveButton" ).click(function() {
+	var id = $("#idInput").va()
+	var adresse = $("addresseInput").val()
+	var interval = $( "#intervalInput" ).val()
+	var date = $("#dateInput").html()
+	
+	
+ 	if( interval > 0 && interval <= 1440)
+ 	{
+ 		io.emit('configInterval', { interval: interval })
+ 	}
+})
