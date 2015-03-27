bdfa5a805e115c7b4f72347371c1989acd24db61
cheroukee <fournierje.info@gmail.com>
2015-03-27 15:03:23 +0000

    Added additional data for data query

--- node-bootstrap/aquariusSensorHelper.js
+++ node-bootstrap/aquariusSensorHelper.js
@@ -112,16 +112,17 @@ module.exports = {
         log("Selecting database", 3)
         connection.query('USE `station_aquarius`;')
 
-        sql = 'SELECT t_Data.data_value AS ReadValue, '+
-        't_Data.data_date AS ReadDate, '+
-        't_VirtualSensor.cloudia_id AS CloudiaSubUnitID, '+
-        't_PhysicalSensor.cloudia_unit_id AS CloudiaUnitID, '+
-        't_PhysicalSensor.physical_name AS PhysicalName, '+
-        't_VirtualSensor.virtual_measure_unit AS UnitType '+
-        'FROM t_Data, t_VirtualSensor, t_PhysicalSensor '+
-        'WHERE t_Data.data_t_virtual = t_VirtualSensor.virtual_id '+
-        'and t_VirtualSensor.virtual_t_physical = t_PhysicalSensor.physical_id '+
-        'and t_Data.data_is_sent = 0 '+
+        sql = 'SELECT t_Data.data_value AS ReadValue, ' +
+        't_Data.idt_Data AS ID, ' +
+        't_Data.data_date AS ReadDate, ' +
+        't_VirtualSensor.cloudia_id AS CloudiaSubUnitID, ' +
+        't_PhysicalSensor.cloudia_unit_id AS CloudiaUnitID, ' +
+        't_PhysicalSensor.physical_name AS PhysicalName, ' +
+        't_VirtualSensor.virtual_measure_unit AS UnitType ' +
+        'FROM t_Data, t_VirtualSensor, t_PhysicalSensor ' +
+        'WHERE t_Data.data_t_virtual = t_VirtualSensor.virtual_id ' +
+        'and t_VirtualSensor.virtual_t_physical = t_PhysicalSensor.physical_id ' +
+        'and t_Data.data_is_sent = 0 ' +
         'ORDER BY t_PhysicalSensor.cloudia_unit_id, t_Data.data_date, t_VirtualSensor.cloudia_id;'
         log(sql, 3)
         return connection.query(sql, callBackToApp)
